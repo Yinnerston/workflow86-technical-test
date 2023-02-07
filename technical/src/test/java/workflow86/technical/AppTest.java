7dc27a255fb32882d54bb413ac5d00d6f73872af
Nathan Yin <nathanzjyin@gmail.com>
2023-02-07 11:44:30 +1100

    added tests javadoc / defined names for test cases

--- technical/src/test/java/workflow86/technical/AppTest.java
+++ technical/src/test/java/workflow86/technical/AppTest.java
@@ -5,16 +5,80 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 /**
- * Unit test for simple App.
+ * Unit test for Technical Challenge
  */
 public class AppTest 
 {
     /**
-     * Rigorous Test :-)
+     * Test module dependencies where the dependencies are not order alphanumerically.
+     * Dependencies retain the ordering defined in the module when not ordered alphanumerically.
+     * Modules:
+     * A: C, D, B
+     * Module Dependencies:
+     * A -> {C, D, B}
+     */
+    public void testModuleDependenciesNonAlphanumericOrdering()
+    {
+        
+    }
+
+    /**
+     * Test correct ordering of dependencies.
+     * Test example one from the technical challenge.
+     * Modules:
+     * A: B, C, D
+     * C: E, F, G
+     * G: H, I
+     * Module Dependencies:
+     * G -> {H, I}
+     * C -> {E, F, H, I, G}
+     * A -> {B, E, F, H, I, G, C, D}
      */
     @Test
-    public void shouldAnswerWithTrue()
+    public void testModuleDependenciesExampleOne()
     {
-        assertTrue( true );
+        //
     }
+
+    /**
+     * Test repeated dependencies are not resolved.
+     * Test example two from the technical challenge.
+     * Modules:
+     * A: B, C, D
+     * C: E, F, G, E
+     * G: H, I
+     * I: D
+     * Module Dependencies:
+     * G -> {H, D, I}
+     * C -> {E, F, H, D, I, G}
+     * A -> {B, E, F, H, D, I, G}
+     */
+    @Test
+    public void testModuleDependenciesExampleTwo()
+    {
+        //
+    }
+
+
+    /**
+     * Test cyclic dependency resolves as error.
+     * Test example three from the technical challenge
+     * Modules:
+     * A: B, C, D
+     * C: E, F, G, E
+     * G: H, I
+     * I: C
+     * Module Dependencies:
+     * G -> {H, D, I}
+     * C -> CyclicDependencyError
+     * A -> CyclicDependencyError
+     */
+    @Test
+    public void testModuleDependenciesExampleThree()
+    {
+        //
+    }
+
+
+
 }
