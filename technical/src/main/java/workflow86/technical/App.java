909a601cd070d366de033fd2d9da6f435696099d
Nathan Yin <nathanzjyin@gmail.com>
2023-02-07 12:55:33 +1100

    defined function definitions for adding modules, getting all modules and getting module dependencies

--- technical/src/main/java/workflow86/technical/App.java
+++ technical/src/main/java/workflow86/technical/App.java
@@ -1,13 +1,47 @@
 package workflow86.technical;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 /**
- * Hello world!
+ * Nathan Yin technical challenge.
  *
  */
 public class App 
 {
+    protected Map<String, List<String>> modules;
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+    }
+
+    /**
+     * Add a module and its direct dependencies.
+     * @param ModuleID
+     * @param dependencies
+     */
+    public void addModule(String ModuleID, List<String> dependencies)
+    {
+        this.modules.put(ModuleID, dependencies);
+    }
+
+    /**
+     * Return Map of moduleIDs: String -> direct dependencies: List<String>
+     * @return
+     */
+    public Map<String, List<String>> getModules()
+    {
+        return this.modules;
+    }
+
+    /**
+     * 
+     * @param ModuleID
+     * @return
+     */
+    public List<String> getModuleDependencies(String ModuleID)
+    {
+        return new ArrayList<String>();
     }
 }
